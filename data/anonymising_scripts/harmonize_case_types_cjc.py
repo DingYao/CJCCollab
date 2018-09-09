6da0e8040712bca65e4d1d77cde695dce7fed23d
Wan Ding Yao <DYWAN@live.com.sg>
2018-09-09 19:06:16 +0800

    Updated directory

--- data/anonymising_scripts/harmonize_case_types_cjc.py
+++ data/anonymising_scripts/harmonize_case_types_cjc.py
@@ -9,7 +9,7 @@ import pandas as pd
 path2data = os.path.join('..', 'original')
 
 # Load data
-case_cjc = pd.read_excel(os.path.join(path2data,'cjc_cases_20178_sample_20180808.xlsx'))
+case_cjc = pd.read_excel(os.path.join(path2data,'cjc_cases_bankruptcy.xlsx'))
 case_type_dic = pd.read_csv(os.path.join(path2data, 'case_type_harmonization.csv'))
 
 
